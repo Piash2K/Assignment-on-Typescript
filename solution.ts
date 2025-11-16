dd8c48ea9cfb64ad53c5e5f9f506afc1037f650b
PIASH <zmnobab1999@gmail.com>
2025-11-16 21:54:56 +0600

    added solution for problem 2

--- solution.ts
+++ solution.ts
@@ -1,9 +1,21 @@
-const formatValue = (value: string | number | boolean):string|number|boolean => {
+const formatValue = (
+  value: string | number | boolean
+): string | number | boolean => {
   if (typeof value === "string") {
     return value.toUpperCase();
   } else if (typeof value === "number") {
     return value * 10;
   } else {
     return !value;
+  }
+};
+
+const getLength = (value: string | any[]): number => {
+  if (typeof value === "string") {
+    return value.length;
+  } else if (Array.isArray(value)) {
+    return value.length;
+  } else {
+    return -1;
   }
 };
